090a51e0dfdf81c42b56b54eeb9284ce8dd9c3ac
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-04-20 01:36:29 -0430

    refactor styles and server upgrade.

--- server.js
+++ server.js
@@ -1,32 +1,20 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
-const youtube = require('youtube-finder')
-const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const port = process.env.PORT || 3000
 
-const port = process.env.PORT || 8080
-
-const client = youtube.createClient({ key: 'AIzaSyAsvEZxR2vYOuxh0-gDFV97HRNaFmP9ZqQ' })
+// templates
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
+// static
 app.use(express.static('public'))
+// routes
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, './public', 'index.html'))
+  res.render('index')
 })
-
-server.listen(3000)
-console.log(`Server running at http://127.0.0.1:${port}/`)
-
-io.on('connection', (socket) => {
-  console.log(`Client connected ${socket.id}`)
-  socket.on('search', (ask) => {
-    console.log(ask)
-    client.search({ part: 'snippet', q: ask, type: 'video' }, (err, data) => {
-      socket.emit('result', data)
-      var idVideo = data.items[0].id.videoId
-      console.log(idVideo)
-    })
-  })
-})
+// server listenign
+server.listen(port, '0.0.0.0')
+console.log(`Server running at http://localhost:${port}/`)
